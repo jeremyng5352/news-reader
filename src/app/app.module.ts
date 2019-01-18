dd9e90bd696df9ad961addacaa0a2265144aebf5
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-01-18 11:58:07 +1000

    create home page + styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
